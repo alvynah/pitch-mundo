58ee6b00f83638c00b2cf270c63672cc445c7ee4
alvynah <alvynahw@gmail.com>
2021-04-26 07:19:22 +0300

    Add column email and flask-login module for user authentication

--- app/main/views.py
+++ app/main/views.py
@@ -11,3 +11,21 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@main.route('/create_new', methods=['POST', 'GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post, user_id=current_user._get_current_object(
+        ).id, category=category, title=title)
+        new_pitch_object.save_p()
+        
+        return redirect(url_for('main.index'))
+
+    return render_template('new_pitch.html', form=form)
