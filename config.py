92e6e5bcf75372bf5cec8b59ef5d64e60d4bf042
alvynah <alvynahw@gmail.com>
2021-04-26 06:33:14 +0300

    Add table users and column password and test wether the password is hashed and checked

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/pitchmundo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cocoh:kitkAt10@localhost/pitchmundo'
 
 
 
