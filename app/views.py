1bfcd6e793f11d64df3e95b7959b773cb06f7b38
alvynah <alvynahw@gmail.com>
2021-04-26 04:36:21 +0300

    Revert to original file structure

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from flask_login import login_required, current_user
+from app import app
+
+# Views
+
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
